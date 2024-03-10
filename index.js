8c6da37c6db156a0990dcfbbc83d78ecc830951c
mohadeev <94206815+mohadeev@users.noreply.github.com>
2024-03-10 23:54:34 +0100

    DF

--- index.js
+++ index.js
@@ -2,78 +2,58 @@ import express from "express";
 import http from "http";
 import cors from "cors";
 import dotenv from "dotenv";
-import { Server, Socket } from "socket.io";
-import Routes from "./routes/routes.js";
-// import bodyParser from "body-parser";
 import dbConnect from "./db/dbConnect.js";
-// import cookieParser from "cookie-parser";
-import socketFuncs from "./socket/socketFuncs.js";
-import test from "./test.js";
-import renderVideo from "./live-funcs/video-handel/rendervideo.js";
-// import renderVideo from "./live-funcs/video-handel/rendervideo.js";
-// import testsFiles from "./testsFiles.js";
+import Routes from "./routes/routes.js";
+import cookieParser from "cookie-parser";
 
-// import session from "express-session";
-// testsFiles();
-//
-dotenv.config();
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 const ORIGIN = process.env.ORIGIN;
-const ORIGINWWW = process.env.ORIGINWWW;
-const ORIGINHTTP = process.env.ORIGINHTTP;
-const ORIGINHTTPWWW = process.env.ORIGINHTTPWWW;
-const ORIGINHTTPS = process.env.ORIGINHTTPS;
-const ORIGINHTTPSWWW = process.env.ORIGINHTTPSWWW;
-//
-// app.use(cookieParser());
-app.use(express.json());
-dbConnect();
-app.use(
-  cors({
-    origin: "*", // use your actual domain name (or localhost), using * is not recommended
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
-    credentials: true,
-  })
+console.log(ORIGIN);
+dotenv.config();
+cors(
+  { "Access-Control-Allow-Origin": "*" },
+  "Access-Control-Allow-Methods: POST, PUT, PATCH, GET, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers: Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization"
 );
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", true);
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  next();
-});
-const server = http.createServer(app);
-dotenv.config();
 
-const io = new Server(server, {
-  cors: {
-    origins: ["*"],
-  },
-});
-dotenv.config();
+dbConnect();
 
+app.use(cookieParser());
+
+// const server = http.createServer(app);
+// const io = new Server(server, {
+//   cors: { origin: "*" },
+// });
+// concect app
 // io.on("connection", (socket) => {
 //   socketFuncs(io, socket);
 // });
+app.use(express.json());
 
-// (() => {
-//   setInterval(() => {
-//     renderVideo();
-//   }, 20000);
-//   renderVideo();
-// })();
-app.use("/", Routes);
-app.get("/", (req, res) => {
-  console.log("updated");
-  res.json("24");
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", `*`);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS,  PUT,PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, scrolling, a_custom_header"
+  );
+  // res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
 });
-app.use("/", test);
+
+app.use("/", Routes);
 
 app.listen(PORT, (err) => {
-  if (err) console.log(err);
-  console.log("Server running on Port ", PORT);
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Server running on Port ", PORT);
+  }
 });
+
+// getUserCountryAndCities();
