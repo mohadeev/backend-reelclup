8b764d6928e5d2844ecd768a69a87e63e5db5391
mohadeev <94206815+mohadeev@users.noreply.github.com>
2024-03-11 01:26:08 +0100

    DG

--- live-funcs/video-handel/timeHandelr.js
+++ live-funcs/video-handel/timeHandelr.js
@@ -1,91 +1,47 @@
-// import { getVideoDurationInSeconds } from "get-video-duration";
-// import VideoTimeReader from "../../routes/video/post/timer.js";
-// import videoModel from "../../db/schema/videoModel.js";
-// import { v4 as uuidv4 } from "uuid";
-// import { fileTypeFromBuffer } from "file-type";
-// // import fileType from "file-type";
-// // import ffprobe from "ffprobe";
-// // import ffprobeStatic from "ffprobe-static";
-// import fs from "fs";
-// import fileTipeHandler from "./fileTipeHandler.js";
-// import createFileFromBuffer from "../../routes/video/post/createFileFromBuffer.js";
-// import path from "path";
-// const __dirname = path.resolve();
+import { getVideoDurationInSeconds } from "get-video-duration";
+import VideoTimeReader from "../../routes/video/post/timer.js";
+import videoModel from "../../db/schema/videoModel.js";
+import { v4 as uuidv4 } from "uuid";
+import fileTipeHandler from "./fileTipeHandler.js";
+import fs from "fs";
+import path from "path";
 
-// const timeHandelr = async (Id, buffer) => {
-//   if (Id) {
-//     videoModel.findOne({ _id: Id }).then(async (video) => {
-//       if (video) {
-//         //     ffprobe(path, { path: ffprobeStatic.path }, (err, info) => {
-//         //       (async () => {
-//         //         if (err) {
-//         //           console.log(err);
-//         //         } else {
-//         //           const numberDuration = info.streams[0].duration;
-//         //           if (typeof numberDuration !== "undefined") {
-//         //             const timeVideo = VideoTimeReader(parseInt(numberDuration));
-//         //             console.log(timeVideo);
-//         //             const filter = {
-//         //               _id: video._id,
-//         //             };
-//         //             const updateUser = {
-//         //               duration: timeVideo,
-//         //             };
-//         //             try {
-//         //               await videoModel.updateOne(filter, updateUser);
-//         //               console.log("updates");
-//         //               // fs.unlinkSync(path);
-//         //               //file removed
-//         //             } catch (error) {}
-//         //           }
-//         //         }
-//         //       })();
-//         //     });
-//         const mainBuffer = Buffer.from(buffer);
-//         const videoFileType = fileTipeHandler(buffer);
-//         console.log("buffer times handeler", videoFileType);
+const __dirname = path.resolve();
 
-//         const fileName = uuidv4() + "." + videoFileType;
-//         console.log(fileName);
-//         const filePath = __dirname + "/" + fileName;
-//         if (fileName && fileName.length) {
-//           fs.writeFile(fileName, mainBuffer, async (err) => {
-//             if (err) {
-//               console.log(err);
-//             } else {
-//               console.log();
-//               fs.access(filePath, fs.constants.F_OK, async (err) => {
-//                 if (err) {
-//                 } else {
-//                   await getVideoDurationInSeconds(filePath).then(
-//                     async (duration) => {
-//                       const timeVideo = VideoTimeReader(duration);
-//                       console.log("timeVideo", timeVideo);
-//                       const filter = {
-//                         _id: video._id,
-//                       };
-//                       const updateUser = {
-//                         duration: timeVideo,
-//                       };
-//                       fs.unlinkSync(filePath);
-//                       try {
-//                         await videoModel.updateOne(filter, updateUser);
-//                       } catch (error) {}
-//                     }
-//                   );
-//                 }
-//               });
-//             }
-//           });
-//         } else {
-//           console.log("file is fucked");
-//         }
-//       }
-//     });
-//   }
-// };
+const timeHandler = async (Id, buffer) => {
+  if (!Id) {
+    return; // Exit early if no ID provided
+  }
 
-// export default timeHandelr;
+  const video = await videoModel.findOne({ _id: Id });
+  if (!video) {
+    return; // Exit early if video not found
+  }
 
-const timeHandelr = async (data) => {};
-export default timeHandelr;
+  const videoFileType = fileTipeHandler(buffer);
+  if (!videoFileType) {
+    console.log("Invalid file type");
+    return;
+  }
+
+  const fileName = uuidv4() + "." + videoFileType;
+  const filePath = path.join(__dirname, fileName);
+
+  try {
+    fs.writeFileSync(filePath, buffer); // Write buffer to file
+
+    const duration = await getVideoDurationInSeconds(filePath);
+    const timeVideo = VideoTimeReader(duration);
+
+    const filter = { _id: video._id };
+    const updateUser = { duration: timeVideo };
+
+    await videoModel.updateOne(filter, updateUser);
+
+    fs.unlinkSync(filePath); // Delete temporary file
+  } catch (error) {
+    console.error("Error processing video:", error);
+  }
+};
+
+export default timeHandler;
